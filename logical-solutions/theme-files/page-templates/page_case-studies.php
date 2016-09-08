7c1251e6b408758877bf153b9bc243d78fd35256
Sloan Sparger <sloansparger@gmail.com>
2016-09-08 13:36:18 -0500

    added new case studies hero

--- logical-solutions/theme-files/page-templates/page_case-studies.php
+++ logical-solutions/theme-files/page-templates/page_case-studies.php
@@ -1,5 +1,21 @@
 <?php
 /*
-Template Name: Case Studies
+Template Name: Case Study
 */
+get_header();
 ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( '/template-parts/hero' );
+			}
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+// get_sidebar();
+get_footer();
