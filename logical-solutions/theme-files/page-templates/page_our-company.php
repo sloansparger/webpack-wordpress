b1265b59007d48114467b35f00fc90447f70d3bc
Sloan Sparger <sloansparger@gmail.com>
2016-09-08 13:35:47 -0500

    added call to new hero template part and calls new data from advanced custom fields plugin

--- logical-solutions/theme-files/page-templates/page_our-company.php
+++ logical-solutions/theme-files/page-templates/page_our-company.php
@@ -7,13 +7,12 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-      <div class="page-hero">
-				<div class="page-hero--image" style="background-image: url(<?php the_post_thumbnail_url() ?>)"></div>
-				<div class="container">
-					<h1 class="page-hero--title uppercase"><?php the_title(); ?></h1>
-					<h4 class="page-hero--blurb"><?php echo get_post_meta( get_the_ID(), 'custom_description' )[0]; ?></h4>
-				</div>
-			</div>
+			<?php
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( '/template-parts/hero' );
+			}
+			?>
 
 			<section class="section-md">
 				<div class="container">
@@ -161,13 +160,6 @@ get_header();
 					</div>
 				</div>
 			</section>
-
-			<?php
-			while ( have_posts() ) : the_post();
-				// get_template_part( 'template-parts/content', 'page' );
-
-			endwhile; // End of the loop.
-			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
