70b928a4a8f9bfc81299833367adacebb5dbb770
Sloan Sparger <sloansparger@gmail.com>
2016-09-08 10:30:48 -0500

    added team members

--- logical-solutions/theme-files/page_inner-page.php
+++ logical-solutions/theme-files/page_inner-page.php
@@ -1,6 +1,6 @@
 <?php
 /* Template Name: Inner Page w/ Transparent Header */
-get_header('');
+get_header();
 ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -13,19 +13,14 @@ get_header('');
 				</div>
 			</div>
 
-
 			<section class="section-md">
 				<div class="container">
-					<div class="row">
-						<div class="col-xs-12">
-							<h3 class="section-title">Since 1995</h3>
-							<p class="split-paragraph">LSi has enjoyed a partnership with a dynamic and innovative company - Automated Logic Corporation (ALC).  leader in the industry for over two decades, ALC continues to be at the forefront of innovative technologies. ALC and LSi - an outstanding combination for innovative building control!<br><br>Now, LSi has partnered with Lenel – the world’s number one provider of electronic access control. With OnGuard, solutions are at your fingertips for systems integration: from card reader and camera installation, to software configuration and integration with third party components, your building has never been so powerful.</p>
-						</div>
-					</div>
+					<h3 class="section-title">Since 1995</h3>
+					<p class="split-paragraph">LSi has enjoyed a partnership with a dynamic and innovative company - Automated Logic Corporation (ALC).  leader in the industry for over two decades, ALC continues to be at the forefront of innovative technologies. ALC and LSi - an outstanding combination for innovative building control!<br><br>Now, LSi has partnered with Lenel – the world’s number one provider of electronic access control. With OnGuard, solutions are at your fingertips for systems integration: from card reader and camera installation, to software configuration and integration with third party components, your building has never been so powerful.</p>
 				</div>
 			</section>
 
-			<section class="section-lg bg-light-grey">
+			<section class="section-lg bg-cool-light-grey">
 				<div class="container">
 					<div class="awards">
 						<div class="award">
@@ -44,6 +39,125 @@ get_header('');
 				</div>
 			</section>
 
+			<section class="section-md repeatable-building-bg">
+				<div class="container">
+					<h3 class="section-title section-title__right">Our Team</h3>
+					<div class="team-members">
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+						<div class="team-member">
+							<img class="team-member--image" src="http://fillmurray.com/250/290" alt="">
+							<div class="team-member--info">
+								<h4 class="team-member--name">Bill Murry</h4>
+								<h5 class="team-member--position">Owner</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 
 			<?php
 			while ( have_posts() ) : the_post();
