ce5b77257c1fa03340b755c39b7ed5b4275b5e68
Sloan Sparger <sloansparger@gmail.com>
2016-09-09 10:10:38 -0500

    working on page content top most section

--- logical-solutions/theme-files/page-templates/page_our-capabilities.php
+++ logical-solutions/theme-files/page-templates/page_our-capabilities.php
@@ -14,6 +14,38 @@ get_header('grey');
     }
     ?>
 
+    <section class="section-md">
+      <div class="container">
+        <h3 class="text-center text-bold text-brand-mid-blue track-out">We’re proud to exceed expectations within a variety of markets, including:</h3>
+        <div class="row">
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="arrow-link">Commercial Office Buildings and Campuses</a>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="arrow-link">Religious Institutions</a>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="arrow-link">Educational Facilities</a>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="arrow-link">Healthcare Buildings and Campuses</a>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="arrow-link">Hospitality and Entertainment Venues</a>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="arrow-link">Industrial Sites</a>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="arrow-link">Pharmaceutical Facilities</a>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="arrow-link">Critical Systems/Data Centers</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
   </main><!-- #main -->
 </div><!-- #primary -->
 <?php
