93b7cb62709742c22f73f72032249387fd8bc40f
Sloan Sparger <sloansparger@gmail.com>
2016-09-09 16:03:03 -0500

    moved capabilites page logic to its own file

--- logical-solutions/index.js
+++ logical-solutions/index.js
@@ -1,21 +1,3 @@
-require('./js/dev-env')
 require('./styles/index.sass')
-import Vue from 'vue'
-
-document.addEventListener('DOMContentLoaded', function() {
-  new Vue({
-    el: '#capabilities',
-    data: {
-      slideNumber: 1,
-      trainingNumber: 1,
-    },
-    methods: {
-      showSlide: function(newSlideNum) {
-        this.slideNumber = newSlideNum
-      },
-      showTraining: function(newTrainingNum) {
-        this.trainingNumber = newTrainingNum
-      }
-    }
-  })
-})
+require('./js/dev-env')
+require('./js/capabilities')
---
+++ logical-solutions/js/capabilites.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+
+document.addEventListener('DOMContentLoaded', function() {
+  new Vue({
+    el: '#capabilities',
+    data: {
+      slideNumber: 1,
+      trainingNumber: 1,
+    },
+    methods: {
+      showSlide: function(newSlideNum) {
+        this.slideNumber = newSlideNum
+      },
+      showTraining: function(newTrainingNum) {
+        this.trainingNumber = newTrainingNum
+      }
+    }
+  })
+})
