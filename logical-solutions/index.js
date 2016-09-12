9758e5cefc2e8b3176302722103435aa781636a5
Sloan Sparger <sloansparger@gmail.com>
2016-09-12 09:23:59 -0500

    added new map, styled, and marker

--- logical-solutions/index.js
+++ logical-solutions/index.js
@@ -1,14 +1,35 @@
 require('./styles/index.sass')
 require('./js/dev-env')
+require('./js/map')
 require('./js/vues')
 
-var map
+
+var map, marker
+var latLng = {
+  lat:32.955873,
+  lng: -96.708707
+}
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: latLng,
+    zoom: 17,
+    zoomControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
+    draggable: false,
+    scrollwheel: false,
+    styles: [{"stylers":[{"hue":"#153265"},{"saturation":35}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":50},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]}]
   })
+
+  marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    title: 'Hello World!'
+  });
 }
 
 window.initMap = initMap
--- logical-solutions/js/contact.js
+++
@@ -1,9 +0,0 @@
-var map
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  })
-  console.log('hiiiii sloan');
-}
