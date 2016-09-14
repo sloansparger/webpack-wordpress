6152c1f66e07bec3b763db32fb61ef563ec6422f
Sloan Sparger <sloansparger@gmail.com>
2016-09-14 15:39:16 -0500

    got modal working with real data, no we only need responsiveness work

--- logical-solutions/js/vues/jobs.js
+++ logical-solutions/js/vues/jobs.js
@@ -7,7 +7,10 @@ module.exports = document.addEventListener('DOMContentLoaded', function() {
       data: {
         catFilter: 'all',
         modalOpen: false,
-        activeModal: ''
+        activeModal: '',
+        currentTitle: 'You are fired!',
+        currentRequirements: ['one', 'two', 'three'],
+        currentDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia consectetur repellat fuga. Repudiandae doloremque eius libero vitae nihil nobis magni aspernatur quis minus sapiente, consectetur adipisci nam sint neque expedita.',
       },
       methods: {
         changeCatFilter: function(filter) {
@@ -21,9 +24,26 @@ module.exports = document.addEventListener('DOMContentLoaded', function() {
           if (this.catFilter === category) return true
           else return false
         },
-        toggleModal: function() {
-          this.modalOpen = !this.modalOpen
-        }
+        openModal: function(newTitle, newDescription, newRequirement) {
+          this.modalOpen = true
+          this.currentTitle = newTitle
+
+          if (newRequirement !== '') {
+            this.currentRequirements = newRequirement.trim().split('\n')
+          } else {
+            this.currentRequirements = false
+          }
+
+          // text area creates three newline characters, convert to break tags
+          var description = '<p>' + newDescription.split('\n\n').join('<br /><br />') + '</p>'
+          var jobDescriptionElem = document.getElementById('jobDescription')
+          jobDescriptionElem.innerHTML = ''
+          // inserts html at begining of #jobDescription
+          jobDescriptionElem.insertAdjacentHTML('afterbegin', description)
+        },
+        closeModal: function() {
+          this.modalOpen = false
+        },
       }
     })
   }
--- logical-solutions/theme-files/page-templates/page_careers.php
+++ logical-solutions/theme-files/page-templates/page_careers.php
@@ -28,9 +28,7 @@ get_header();
 					<h3 class="section-title">Open Positions</h3>
 					<div class="row mb10">
 						<div class="col-md-3">
-							<p class="job--category" @click="changeCatFilter('all')" v-bind:class="{'job--category__active': isCurrentFilter('all')}">
-								All Categories  <span class="job--category-count">(<?php echo $totalJobs ?>)</span>
-							</p>
+							<p class="job--category" @click="changeCatFilter('all')" v-bind:class="{'job--category__active': isCurrentFilter('all')}">All Categories  <span class="job--category-count">(<?php echo $totalJobs ?>)</span></p>
 						</div>
 
 						<?php
@@ -40,74 +38,60 @@ get_header();
 							<div class="col-md-3">
 								<p class="job--category"
 									 @click="changeCatFilter('<?php echo strtolower($currentCategory) ?>')"
-									 v-bind:class="{'job--category__active': isCurrentFilter('<?php echo strtolower($currentCategory) ?>')}"
-									 >
+									 v-bind:class="{'job--category__active': isCurrentFilter('<?php echo strtolower($currentCategory) ?>')}">
 									<?php echo $currentCategory; ?>
 									<span class="job--category-count">(<?php echo $categoryCounts[$currentCategory]; ?>)</span>
 								</p>
 							</div>
 						<?php } ?>
-
 					</div>
+
 					<div class="row">
-
-						<?php if ( $jobs->have_posts() ) : while ( $jobs->have_posts() ) : $jobs->the_post();?> <!-- start nested loop -->
-									<div class="col-md-4" v-show="matchesCurrentFilter('<?php echo strtolower(get_the_terms(get_the_ID(), 'job-category')[0]->name) ?>')">
-										<div class="job--preview">
-											<h4 class="job--preview-title mb0"><?php the_title(); ?></h4>
-											<span v-text="'<?php the_date(); ?>' | timeago"></span><br>
-											<p class="job--excerpt mt2"><?php
-												$desc = get_field('description');
-												if (strlen($desc) > 100) echo substr(get_field('description'), 0, 99) . '...';
-												else echo $desc
-											?></p>
-											<button class="btn btn__blue btn__small" @click="toggleModal">Learn More</button>
-										</div>
-									</div>
-
+						<?php if ( $jobs->have_posts() ) : while ( $jobs->have_posts() ) : $jobs->the_post();
+									$desc = get_field('description', false, false);
+									$req = get_field('requirements', false, false);
+						?> <!-- start nested loop -->
+							<div class="col-md-4" v-show="matchesCurrentFilter('<?php echo strtolower(get_the_terms(get_the_ID(), 'job-category')[0]->name) ?>')">
+								<div class="job--preview">
+									<h4 class="job--preview-title mb0"><?php the_title(); ?></h4>
+									<span v-text="'<?php the_date(); ?>' | timeago"></span>
+									<br/>
+									<p class="job--excerpt mt2"><?php if (strlen($desc) > 100) echo substr($desc, 0, 99) . '...'; else echo $desc; ?></p>
+									<button class="btn btn__blue btn__small" @click="openModal('<?php the_title(); ?>', '<?php echo $desc; ?>', '<?php echo $req; ?>')">Learn More</button>
+								</div>
+							</div>
 						<?php endwhile; else : endif;?> <!--  end the loop -->
 					</div>
 				</div>
 			</section>
 
-			<!-- <modal class="modal" v-bind:class="{'modal-is-open': modalOpen}" job-title="Example Job"></modal> -->
-			<modal class="modal" v-bind:class="{'modal__open': modalOpen}" title="Example Job">
+			<div class="modal" v-bind:class="{'modal__open': modalOpen}">
+				<div class="container relative">
+	        <div class="modal--exit" @click="closeModal"></div>
+	      </div>
 				<div class="container">
-					<h1 class="text-brand-blue uppercase text-thin mb4">Lorem ipsum dolor sit amet</h1>
+					<h1 class="text-brand-blue uppercase text-thin mb4">{{currentTitle}}</h1>
 				</div>
-				<div class="container modal--content">
-					<div class="row">
-						<div class="col-md-6">
-							<p class="text-bold text-brand-green mt0 mb4">Job Description</p>
-							<div class="job--description">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum aliquet congue. In ac ipsum ac nunc auctor interdum at sit amet mi. Vivamus lectus ipsum, hendrerit fermentum felis sit amet, placerat tristique sem. Sed gravida id risus ac blandit. Etiam luctus finibus fringilla. Maecenas vehicula, tellus nec vestibulum dapibus, ante enim viverra metus, vel sollicitudin odio risus ac ligula. Sed rutrum augue eget quam vulputate, nec consequat mi semper. Praesent commodo ullamcorper lectus vel iaculis. Vestibulum facilisis turpis purus, id aliquam nisl aliquam quis. Praesent fringilla condimentum purus, quis dapibus leo vestibulum at. Proin eget luctus purus, at vulputate ipsum.</p>
-								<br/>
-								<p>Phasellus et pharetra arcu. Pellentesque eu augue sed turpis aliquam cursus. Praesent maximus, orci ut lacinia rutrum, massa lacus fermentum tellus, nec varius enim lectus nec velit. Sed pharetra risus et nulla aliquet consequat. Suspendisse venenatis elit sit amet risus accumsan, et molestie enim efficitur. Integer eget cursus nisl. In ut ullamcorper ex. Morbi vitae imperdiet sapien, sit amet sollicitudin.</p>
-								<br/>
-								<p>Phasellus et pharetra arcu. Pellentesque eu augue sed turpis aliquam cursus. Praesent maximus, orci ut lacinia rutrum, massa lacus fermentum tellus, nec varius enim lectus nec velit. Sed pharetra risus et nulla aliquet consequat. Suspendisse venenatis elit sit amet risus accumsan, et molestie enim efficitur. Integer eget cursus nisl. In ut ullamcorper ex. Morbi vitae imperdiet sapien, sit amet sollicitudin.</p>
-								<br/>
-								<p>Phasellus et pharetra arcu. Pellentesque eu augue sed turpis aliquam cursus. Praesent maximus, orci ut lacinia rutrum, massa lacus fermentum tellus, nec varius enim lectus nec velit. Sed pharetra risus et nulla aliquet consequat. Suspendisse venenatis elit sit amet risus accumsan, et molestie enim efficitur. Integer eget cursus nisl. In ut ullamcorper ex. Morbi vitae imperdiet sapien, sit amet sollicitudin.</p>
-								<br/>
-								<p>Phasellus et pharetra arcu. Pellentesque eu augue sed turpis aliquam cursus. Praesent maximus, orci ut lacinia rutrum, massa lacus fermentum tellus, nec varius enim lectus nec velit. Sed pharetra risus et nulla aliquet consequat. Suspendisse venenatis elit sit amet risus accumsan, et molestie enim efficitur. Integer eget cursus nisl. In ut ullamcorper ex. Morbi vitae imperdiet sapien, sit amet sollicitudin.</p>
-								<br/>
-								<p>Phasellus et pharetra arcu. Pellentesque eu augue sed turpis aliquam cursus. Praesent maximus, orci ut lacinia rutrum, massa lacus fermentum tellus, nec varius enim lectus nec velit. Sed pharetra risus et nulla aliquet consequat. Suspendisse venenatis elit sit amet risus accumsan, et molestie enim efficitur. Integer eget cursus nisl. In ut ullamcorper ex. Morbi vitae imperdiet sapien, sit amet sollicitudin.</p>
+				<div class="modal--content-height-set">
+					<div class="container modal--content">
+						<div class="row">
+							<div class="col-md-6">
+								<p class="text-bold text-brand-green mt0 mb4">Job Description</p>
+								<div class="job--description" id="jobDescription">
+								</div>
 							</div>
-						</div>
-						<div class="col-md-5 col-md-offset-1">
-							<p class="text-bold text-brand-green mb4">Job Requirements</p>
-							<ul class="job--requirements">
-								<li class="job--requirement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum aliquet congue.</li>
-								<li class="job--requirement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum aliquet congue.</li>
-								<li class="job--requirement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum aliquet congue.</li>
-								<li class="job--requirement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum aliquet congue.</li>
-								<li class="job--requirement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum aliquet congue.</li>
-								<li class="job--requirement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum aliquet congue.</li>
-							</ul>
+							<div class="col-md-5 col-md-offset-1">
+								<p class="text-bold text-brand-green mb4">Job Requirements</p>
+								<ul class="job--requirements">
+									<li class="job--requirement" v-for="requirement in currentRequirements" track-by="$index">
+										{{requirement}}
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>
-
-			</modal>
+			</div>
 
 		<?php endwhile; else : endif;?> <!--  end the loop -->
 		</main><!-- #main -->
