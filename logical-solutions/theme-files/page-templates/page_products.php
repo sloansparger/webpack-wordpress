dd49cb5920a475ecfd13d175872627f1a4723679
amphrony <chase.mcdonald626@gmail.com>
2016-09-15 14:29:57 -0500

    added products page, blue button

--- logical-solutions/theme-files/page-templates/page_products.php
+++ logical-solutions/theme-files/page-templates/page_products.php
@@ -15,6 +15,210 @@ get_header();
 			}
 			?>
 
+			<section class="section-md repeatable-building-bg">
+				<div class="container">
+					<h3 class="section-title section-title__right">Software</h3>
+					<div class="products">
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+            <div class="products--contact-btn-container">
+              <a class="btn btn__blue products--contact-btn" href="/contact/">Contact Us</a>
+            </div>
+
+					</div>
+				</div>
+
+				<div class="container">
+					<h3 class="section-title section-title__right">Hardware</h3>
+					<div class="products">
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+						<div class="product">
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+                <h3 class="product--name text-white">Product Murray</h3>
+              </div>
+              <div class="product--info-container bg-mid-grey">
+                <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
+              </div>
+            </div>
+
+            <div class="products--contact-btn-container">
+              <a class="btn btn__blue products--contact-btn" href="/contact/">Contact Us</a>
+            </div>
+
+					</div>
+				</div>
+			</section>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 </div>
