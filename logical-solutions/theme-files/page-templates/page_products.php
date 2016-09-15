475f14790f2753eca9a96c6975fc7f27bf028c37
Sloan Sparger <sloansparger@gmail.com>
2016-09-15 15:39:28 -0500

    refactoring product css and adding animation

--- logical-solutions/theme-files/page-templates/page_products.php
+++ logical-solutions/theme-files/page-templates/page_products.php
@@ -15,204 +15,244 @@ get_header();
 			}
 			?>
 
-			<section class="section-md repeatable-building-bg">
+			<section class="section-md">
 				<div class="container">
-					<h3 class="section-title section-title__right">Software</h3>
+					<h3 class="section-title">Software</h3>
 					<div class="products">
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
             <div class="products--contact-btn-container">
-              <a class="btn btn__blue products--contact-btn" href="/contact/">Contact Us</a>
+              <a class="btn btn__blue products--contact-btn uppercase" href="/contact/">Contact Us</a>
             </div>
 
 					</div>
 				</div>
 
 				<div class="container">
-					<h3 class="section-title section-title__right">Hardware</h3>
+					<h3 class="section-title">Hardware</h3>
 					<div class="products">
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								<a href="#" class="product--learn-more btn btn__white">Learn More</a>
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								button.btn.btn__white
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
+								<div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
+								button.btn.btn__white
               </div>
-              <div class="product--info-container bg-mid-grey">
+              <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
 
             <div class="products--contact-btn-container">
-              <a class="btn btn__blue products--contact-btn" href="/contact/">Contact Us</a>
+              <a class="btn btn__blue products--contact-btn uppercase" href="/contact/">Contact Us</a>
             </div>
 
 					</div>
