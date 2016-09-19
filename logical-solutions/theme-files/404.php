4061f13976fe4bbbc4b7e002458a7cee93a3e7a9
Sloan Sparger <sloansparger@gmail.com>
2016-09-19 12:52:36 -0500

    added 404 page

--- logical-solutions/theme-files/404.php
+++ logical-solutions/theme-files/404.php
@@ -7,56 +7,23 @@
  * @package Logical_Solutions
  */
 
-get_header(); ?>
+get_header('grey'); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<section class="section__404 m0">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'logical-solutions' ); ?></h1>
-				</header><!-- .page-header -->
+					<div class="container">
+						<div class="fourzerofour">
+							<h1 class="fourzerofour--title text-brand-dark-blue text-bold">404</h1>
+							<p class="fourzerofour--text">
+								Sorry, but the page you are looking for cannot be found.<br class="fourzerofour--break">
+								We’re working on a logical solution for this problem.
+							 </p>
+						</div>
+					</div>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'logical-solutions' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( logical_solutions_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'logical-solutions' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'logical-solutions' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
