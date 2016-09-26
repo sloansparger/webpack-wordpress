cd04a1d923ed88955c0665130132f326d797200d
Sloan Sparger <sloansparger@gmail.com>
2016-09-26 07:55:15 -0500

    finished styling for traing page

--- logical-solutions/theme-files/page-templates/page_training.php
+++ logical-solutions/theme-files/page-templates/page_training.php
@@ -21,49 +21,44 @@ get_header('grey');
 				    <div class="col-md-7">
 				      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper, felis nec pellentesque vehicula, turpis nibh placerat eros, eu pharetra tortor ligula consectetur velit. Fusce dignissim lobortis mollis. Aenean imperdiet volutpat accumsan. Sed sem turpis, imperdiet a aliquam sed, posuere vel nisl. Suspendisse nec magna non elit elementum bibendum nec quis metus. Aliquam elementum ex in justo tincidunt tincidunt. Cras rutrum nulla et risus laoreet euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras tincidunt et leo vel viverra.</p>
               <p>Fusce id tortor nec sem pulvinar placerat sit amet non ante. Maecenas scelerisque lacus sit amet aliquam congue. Donec fermentum, odio ut scelerisque mollis, nisi tortor vehicula leo, sed placerat quam justo non nibh. Donec feugiat mi vel ex luctus vehicula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer sagittis ut neque eu blandit. Vivamus a imperdiet mauris, quis luctus eros. Suspendisse pharetra mi quis eleifend euismod. Donec est est, ultricies et mollis id, pretium nec dui.</p>
+							<div class="training--divider"></div>
 				    </div>
 				    <div class="col-md-3 col-md-offset-2">
-              <a href="/products" class="btn btn__blue mb5">Register Now</a>
+              <a href="#register" class="btn btn__blue mb5">Register Now</a>
               <h1 class="uppercase text-thin mt0">Schedule</h1>
 				    </div>
 				  </div>
 				</div>
 			</section>
 
-      <section class="relative">
-        <div class="training--info-section">
-          <h1 class="text-thin text-white uppercase mt0">This Session Limit</h1>
-          <p class="text-white">for 00.00.2016</p>
-          <h1 class="text-white">45</h1>
-          <h1 class="text-white">Current Enrollment</h1>
-        </div>
+      <section>
         <div class="container">
           <h3 class="section-title image-section--title">Next Session</h3>
+					<h1 class="uppercase text-thin track-out mt0 mb5">Wednesday, August 10, 2016</h1>
           <div class="row">
-            <div class="col-md-6">
-              <h1 class="uppercase text-thin track-out mt0 mb5">Wednesday, August 10, 2016</h1>
-              <div class="row">
-                <div class="col-md-6">
-                  <p class="text-bold mb0 uppercase">Session Details</p>
-                  <p>10:00 AM - 2:00 PM</p>
-                  <br>
-                  <address class="mb3"><p>
-                    407 International Pkwy<br/>
-                    Suite 406<br/>
-                    Richardson, Texas 75081
-                  </p></address>
-                </div>
-                <div class="col-md-6">
-                  <p class="text-bold mb0 uppercase">Registration Deadline</p>
-                  <p>August 01, 2016</p>
-                </div>
-              </div>
+            <div class="col-md-4">
+              <p class="text-bold mb0 uppercase">Session Time</p>
+              <p>10:00 AM - 2:00 PM</p>
+						</div>
+						<div class="col-md-4">
+							<p class="text-bold mb0 uppercase">Session Location</p>
+              <a class="text-brand-dark-blue" href="https://www.google.com/maps/place/407+International+Pkwy+%23406,+Richardson,+TX+75081/@32.9548219,-96.7122626,17z/data=!3m1!4b1!4m5!3m4!1s0x864c1f0332305ac3:0x12c5f11a6424ee1!8m2!3d32.9548219!4d-96.7100739">
+								<address class="mb3"><p>
+              	  407 International Pkwy<br/>
+              	  Suite 406<br/>
+              	  Richardson, Texas 75081
+              	</p></address>
+							</a>
+						</div>
+						<div class="col-md-4">
+              <p class="text-bold mb0 uppercase">Registration Deadline</p>
+              <p>August 01, 2016</p>
             </div>
           </div>
         </div>
       </section>
 
-      <section class="section-md bg-cool-light-grey">
+      <section class="section-md bg-cool-light-grey" id="register">
         <div class="container">
           <h3 class="section-title">Register for Training</h3>
           <?php echo do_shortcode( '[contact-form-7 id="68" title="Training Form"]' ); ?>
