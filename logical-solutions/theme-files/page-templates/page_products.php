d17e1e029330685a9fa42c8960e35af2d5a85e26
amphrony <chase.mcdonald626@gmail.com>
2016-09-27 10:16:20 -0500

    started products page php.. want filter by category

--- logical-solutions/theme-files/page-templates/page_products.php
+++ logical-solutions/theme-files/page-templates/page_products.php
@@ -8,30 +8,51 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php
-			while ( have_posts() ) {
-				the_post();
-				get_template_part( '/template-parts/hero' );
-			}
-			?>
+			<?php while ( have_posts() ) { the_post(); get_template_part( '/template-parts/hero' ); } ?>
+
+      <?php
+        $products = new WP_Query( array( 'post_type' => 'lsi_product' ) );
+        $allCategories = [];
+        $totalProducts = 0;
+        if ( $products->have_posts() ) : while ( $products->have_posts() ) : $products->the_post();
+          array_push($allCategories, get_the_terms(get_the_ID(), 'product-category')[0]->name);
+          $totalProducts++; 
+          $categoryCount = array_count_values($allCategories);
+          $mainCategories = array_values(array_unique($allCategories));
+        endwhile; endif;
+      ?>
 
 			<section class="section-md" id="products">
+
+        <?php
+          for($i = 0; $i < count($mainCategories); $i++) {
+            $currentCategory = $mainCategories[$i];
+        ?>
+
 				<div class="container">
-					<h3 class="section-title">Software</h3>
+					<h3 class="section-title"><?php echo strtoupper($currentCategory); ?></h3>
 					<div class="products">
 
+            <?php
+              for($i = 0; $i < count($totalProducts); $i++) {
+                $currentProduct = $
+            ?>
+
 						<div class="product">
-              <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
-								<div class="product--overlay"></div>
+              <div class="product--image"  style="background-image: url(http://fillmurray.com/500/580)">
+                <div class="product--overlay"></div>
                 <h3 class="product--name text-white">Product Murray</h3>
-								<button class="product--learn-more btn btn__white" @click="openModal('Title', 'http://fillmurray.com/500/580', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel voluptate accusantium voluptates. Eligendi cupiditate sunt accusamus in ut, nostrum quis atque consequatur blanditiis, ratione dolor iure ad possimus nobis vel!')">
-									Learn More
-								</button>
+                <button class="product--learn-more btn btn__white" @click="openModal('Title', 'http://fillmurray.com/500/580', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel voluptate accusantium voluptates. Eligendi cupiditate sunt accusamus in ut, nostrum quis atque consequatur blanditiis, ratione dolor iure ad possimus nobis vel!')">
+                  Learn More
+                </button>
               </div>
               <div class="product--info-container">
                 <h5 class="product--info text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi </h5>
               </div>
             </div>
+
+            <?php } ?>
+
 
 						<div class="product">
               <div class="product--image"  style="background-image: url(http://fillmurray.com/250/290)">
